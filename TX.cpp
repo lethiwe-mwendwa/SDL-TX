e044f8c8c0b3cadb6025690f9024931d423c2e4c
Lethiwe Mwendwa <lethiwe.mwendwa@gmail.com>
2023-06-26 21:53:13 +0100

    baby steps

--- TX.cpp
+++ TX.cpp
@@ -1,3 +1,49 @@
 #include "TX.h"
 
+// Destroys renderer and window then quits SDL
+void TX_WindowDest(SDL_Renderer* renderer, SDL_Window* window) {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
 
+// Returns a RectRectFromImage
+SDL_Rect TX_RectFromImage(float SCREEN_WIDTH, float SCREEN_HEIGHT, SDL_Surface* imageSurface, float widthMult, float heightMult, float x, float y) {
+	int imageWidth = imageSurface->w;
+	int imageHeight = imageSurface->h;
+	SDL_Rect rect{};
+
+	if (SCREEN_HEIGHT > SCREEN_WIDTH) {
+		rect.w = SCREEN_WIDTH * heightMult;
+		rect.h = (rect.w * imageHeight) / imageWidth;
+	}
+	else {
+		rect.h = SCREEN_HEIGHT * widthMult;
+		rect.w = (rect.h * imageWidth) / imageHeight;
+	}
+
+	rect.x = x;
+	rect.y = y;
+
+	return rect;
+}
+
+// Destroys renderer and window then quits SDL
+void TX_RenderImage(SDL_Renderer* renderer, float SCREEN_WIDTH, float SCREEN_HEIGHT, char* IMAGE_PATH, float widthMult, float heightMult, float x, float y) {
+
+	SDL_Surface* imageSurface = NULL;
+	imageSurface = SDL_LoadBMP(IMAGE_PATH);
+	if (NULL == imageSurface) {
+		cout << "SDL error BRO WHAT" << SDL_GetError();
+	}
+
+	SDL_Texture* imageTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+	SDL_Rect rect = TX_RectFromImage(SCREEN_WIDTH, SCREEN_HEIGHT, imageSurface, widthMult, heightMult, x, y);
+
+	SDL_FreeSurface(imageSurface);
+
+	SDL_RenderCopy(renderer, imageTexture, NULL, &rect);
+	SDL_DestroyTexture(imageTexture);
+	SDL_RenderPresent(renderer);
+
+}
--- TX.h
+++ TX.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "SDL.h"
+#include <iostream>
+using namespace std;
+
+// Destroys renderer and window then quits SDL
+void TX_WindowDest(SDL_Renderer* renderer, SDL_Window* window);
+
+// Returns a RectRectFromImage
+SDL_Rect TX_RectFromImage(float SCREEN_WIDTH, float SCREEN_HEIGHT, SDL_Surface* imageSurface, float widthMult, float heightMult, float x, float y);
+
+// Destroys renderer and window then quits SDL
+void TX_RenderImage(SDL_Renderer* renderer, float SCREEN_WIDTH, float SCREEN_HEIGHT, char* IMAGE_PATH, float widthMult, float heightMult, float x, float y);
